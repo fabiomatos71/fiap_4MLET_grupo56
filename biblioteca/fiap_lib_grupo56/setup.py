6f007a179f4f735310892f0208b874389404529a
Fabio Vargas Matos <fabiomatos@baneses.com.br>
2025-01-21 15:09:21 -0300

    ...

--- biblioteca/fiap_lib_grupo56/setup.py
+++ biblioteca/fiap_lib_grupo56/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='fiap_lib_grupo56',
-   version='0.3.22',
+   version='0.3.23',
    package_dir={"" : "src"},
    packages=find_packages(
         where='src',
