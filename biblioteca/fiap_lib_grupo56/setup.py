01e5f202a74e28db258c56de0291f0f4a0f876da
Fabio Vargas Matos <fabiomatos@baneses.com.br>
2025-01-21 12:15:19 -0300

    resolvendo erro dos arquivos .csv

--- biblioteca/fiap_lib_grupo56/setup.py
+++ biblioteca/fiap_lib_grupo56/setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
    name='fiap_lib_grupo56',
    version='0.3.4',
+   package_dir={"" : "src"},
    packages=find_packages(),
+#    packages=['modelo_dados', 'site_embrapa'],
    description='Biblioteca para o trabalho Tech Challenge do grupo 56 da 4MLET',
    author='Fabio Vargas Matos',
    author_email='fabiomatos@baneses.com.br',
