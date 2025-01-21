335650c9e3331d3ced7788f5cfcb5ba016ba5d98
Fabio Vargas Matos <fabiomatos@baneses.com.br>
2025-01-21 13:57:58 -0300

    ...

--- biblioteca/fiap_lib_grupo56/src/site_embrapa/site_embrapa.py
+++ biblioteca/fiap_lib_grupo56/src/site_embrapa/site_embrapa.py
@@ -25,20 +25,32 @@ class SiteEmbrapa:
 
     def inicializa_repositorios(self):
         import os
+        import site_embrapa.logs as logs
 
-        print(f"os.getcwd():{os.getcwd()}")
-        print("---------------- conteudo da pasta corrente ------------")
-        print(f"os.listdir():{os.listdir()}")
-        print("---------------- fim conteudo da pasta corrente ------------")
-        try:
-            with as_file(files("site_embrapa.arquivos_csv").joinpath("producao.csv")) as caminho_csv:
-                print(f"caminho_csv:{caminho_csv}")
-                with open(caminho_csv, mode='r', encoding='utf-8') as file:
-                    content = file.read() 
-                    print(content)
-        except FileNotFoundError: 
-            print("File not found. Check the file path and filename.") 
-        except PermissionError: print("Permission denied. Check file permissions.")
+        # Setup logging
+        logger = logs.setup_logging()
+
+        # Log application start
+        logger.info('Inicio aplicação')
+
+        logger.debug('This is a debug message')
+        logger.info('Application is running normally')
+        logger.warning('Something might be wrong')
+        logger.error('Attempted to divide by zero')
+
+        # print(f"os.getcwd():{os.getcwd()}")
+        # print("---------------- conteudo da pasta corrente ------------")
+        # print(f"os.listdir():{os.listdir()}")
+        # print("---------------- fim conteudo da pasta corrente ------------")
+        # try:
+        #     with as_file(files("site_embrapa.arquivos_csv").joinpath("producao.csv")) as caminho_csv:
+        #         print(f"caminho_csv:{caminho_csv}")
+        #         with open(caminho_csv, mode='r', encoding='utf-8') as file:
+        #             content = file.read() 
+        #             print(content)
+        # except FileNotFoundError: 
+        #     print("File not found. Check the file path and filename.") 
+        # except PermissionError: print("Permission denied. Check file permissions.")
 
 
 
